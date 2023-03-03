69887e2cd81b0e55ad9ebb79975a0d50fc0560d4
davidlegarre <davidls920@gmail.com>
2023-03-03 01:32:12 +0100

    Webdriver started

--- Scrapper/top250.py
+++ Scrapper/top250.py
@@ -1,12 +1,21 @@
+import sys
+######
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+######
+sys.path.append(".")
+
+from Classes.movie import *
 
 URL = "https://www.imdb.com/chart/top/"
 
-driver = webdriver.Chrome("usr/lib/chromium-browser/chromedriver")
+driver = webdriver.Chrome()
 titles = []
 titles_id = []
 
 
 if __name__=='__main__':
+    print("Hello World")
+    movie = Movie("Patata", "450903",2)
+    print(movie)
