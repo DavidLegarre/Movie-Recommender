065da24dd508d00d0c015759d9e751b89a687bfa
davidlegarre <davidls920@gmail.com>
2023-03-04 23:52:50 +0100

    Movie web scrapper fixed (genres not working correctly)

--- Scrapper/imdb_scrapper.py
+++ Scrapper/imdb_scrapper.py
@@ -1,8 +1,11 @@
 import re
+import time
 ###
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import pandas as pd
+from fake_useragent import UserAgent
 ###
 
 
@@ -11,7 +14,14 @@ def movie_web_scrapper(url):
     title = ""
     rating = 0
     genres = []
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    ua = UserAgent()
+    userAgent = ua.chrome
+    chrome_options.add_argument(f'user-agent={userAgent}')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--allow-running-insecure-content')
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.get(url)
     content = driver.page_source
@@ -19,24 +29,36 @@ def movie_web_scrapper(url):
 
     id_ = "(?<=\/title\/)[^\/]+"
 
+    with open("content.html", "w", encoding='utf-8') as f:
+        f.write(content)
 
     # Obtain Id
     match = re.search(id_, url)
     id = match.group()
 
     # Obtain Title
-    title = soup.find('span', class_='sc-afe43def-1 fDTGTb').text
+    # Remove " - IMDb" from the title
+    pattern = " - IMDb"
+    title = soup.find('title').text
+    title = title.replace(pattern, "")
+    pattern = "\ \([0-9]*\)"
+    title = re.sub(pattern, "", title)
 
-    # Find all ratings   
-    genres_span = soup.find_all('span', class_='ipc-chip__text')
-    for genre in genres_span:
+    # Find all genres
+    genres_a = soup.find("div", {"data-testid": "genres"})
+    genres_a = genres_a.find_all('a')
+    for genre in genres_a:
         genres.append(genre.text)
-    
-    # Obtain rating
-    rating = float(soup.find('span', class_='sc-e457ee34-1 squoh').text)
 
+    # Obtain rating
+    # rating = float(soup.find('span', class_='sc-e457ee34-1 squoh').text)
+    rating_ = soup.find(
+        "div", attrs={"data-testid": "hero-rating-bar__aggregate-rating__score"}).text
+    rating = float(rating_[:-3])
     # Drop last input to genres "back to top"
-    genres.pop()
+    # genres.pop()
+
+    driver.quit()
 
     return id, title, rating, genres
 
@@ -44,3 +66,6 @@ def movie_web_scrapper(url):
 if __name__ == '__main__':
     output = movie_web_scrapper("https://www.imdb.com/title/tt13833688")
     print(output)
+
+    output2 = movie_web_scrapper("https://www.imdb.com/title/tt0068646")
+    print(output2)
